ba94d70fae6d2ab6265a1cd582ca6888e93a8ca8
arielszabo <arielszabo@gmail.com>
2018-09-26 12:52:10 +0300

    add another test with generator

--- ariels_utils/tests/__init__.py
+++ ariels_utils/tests/__init__.py
@@ -1,5 +1,6 @@
 from ariels_utils.ariels_utils import IteratorReStarter
 
+
 class CustomIterator(object):
     """Custom Iterator for testing"""
     def __init__(self, x):
@@ -17,9 +18,18 @@ class CustomIterator(object):
             raise StopIteration
 
 
+def custom_generator(list_of_values):
+    for i in list_of_values:
+        yield i+1
+
+
 def test_IteratorReStarter():
-    iterator_object = CustomIterator(12)
     new = IteratorReStarter(CustomIterator, [1])
     # run over the iterator once. An iterator will remain empty after one full iteration.
+    list(new)
+    assert list(new) != []
+
+    new = IteratorReStarter(custom_generator, [[1, 2, 3, 4]])
+    # run over the generator once. An generator will remain empty after one full iteration.
     list(new)
     assert list(new) != []
